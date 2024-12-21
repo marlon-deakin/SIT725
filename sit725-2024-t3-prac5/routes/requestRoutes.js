6bd4e8d9c46fc1660295a605994ba26e237a0708
marlon-deakin <s218516527@deakin.edu.au>
2024-12-21 21:13:02 +1100

    clean up folder for prac 6

--- sit725-2024-t3-prac5/routes/requestRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const requestController = require('../controllers/requestController');
-
-router.get('/api/requests', requestController.getAllRequests);
-
-module.exports = router;
