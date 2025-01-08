79594a917584f4f3f78ae1f9c091540d22a18ec3
marlon-deakin <s218516527@deakin.edu.au>
2025-01-08 20:38:01 +1100

    initial check-in for prac7

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const taskRoutes = require("./routes/taskRoutes");
 const initializeMongoServer = require("./config/config");
+const http = require('http');
+const io = require('socket.io')(http);
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use("/api", taskRoutes);
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  
+  socket.on('disconnect', () => {
+      console.log('User disconnected');
+  });
+
+  setInterval(() => {
+      socket.emit('number', Math.floor(Math.random() * 10));
+  }, 1000);
+});
 
 initializeMongoServer()
   .then(() => {
